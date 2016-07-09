9f5aa4d125172e50e5409b31a44710b18ee5e444
Chris Morris <morriswchris@gmail.com>
2016-07-08 23:21:47 -0400

    added in colour

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function($){
     return Math.ceil(Math.random() * (max - 0) + 0);
   }
 
+  function type_text(curse) {
+    $("#curse").typed({
+      strings: [curse],
+      showCursor: false,
+      typeSpeed: 10,
+    });
+  }
+
   function get_curses(){
     $.ajax({
       url: "https://raw.githubusercontent.com/morriswchris/proast/master/curses.json",
@@ -12,7 +20,7 @@ $(function($){
         curses = data.curses;
         var index = random_index(curses.length);
         var curse = curses[index];
-        $("#curse").text(curse);
+        type_text(curse);
       }
     });
   }
