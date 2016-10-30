7ba8b1ca1d65cb60cb74bd7a7b29edc99f52faaf
“sschadwick” <sschadwick@gmail.com>
2016-10-30 15:51:53 -0700

    using path.join

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var curses = require('./curses.json').curses;
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/random', function(req, res) {
   res.json(curses[Math.floor(Math.random() * curses.length)]);
